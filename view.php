61ae7239d5709197cd2c0f4edf440ffe19cfcebb
StGrv <65493257+StGrv@users.noreply.github.com>
2020-09-30 15:21:35 +0300

    The view file
    Displaying unique records for each row using $id params

---
+++ view.php
@@ -0,0 +1,9 @@
+<?php
+use backend\models\Categories ;
+?>
+
+<strong> ID:</strong> <?=$categories->id ?>
+<strong> Mark:</strong> <?=$categories->mark ?>
+<strong> Model:</strong> <?=$categories->model ?>
+<strong> Engine:</strong> <?=$categories->engine ?>
+<strong> Transmission:</strong> <?=$categories->transmission ?>
