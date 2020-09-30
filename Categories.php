04351c83c983044a6c6b7ad35377553d700e848b
StGrv <65493257+StGrv@users.noreply.github.com>
2020-09-30 15:17:26 +0300

    The Categories model

---
+++ Categories.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+class Categories extends \yii\db\ActiveRecord
+{
+    
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    public function rules()
+    {
+        return [
+            [['mark', 'model'], 'required'],
+            [['mark', 'model', 'engine', 'transmission'], 'string', 'max' => 255],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'mark' => 'Mark',
+            'model' => 'Model',
+            'engine' => 'Engine',
+            'transmission' => 'Transmission',
+        ];
+    }
+
+    
+} // End
