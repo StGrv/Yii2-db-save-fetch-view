2954228ce4e84e9ec3800b0622713363bb8777cc
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-07 12:15:21 +0300

    The Index file
    This file contains the view of the fetched data from the DB using @foreach cycle.

--- index.php
+++ index.php
@@ -3,17 +3,14 @@ use backend\models\Categories ;
 use yii\helpers\Html ;
 use yii\helpers\Url ;
 
-
 foreach ($categories as $category) { ?>
     
-<strong> Mark: </strong> <?= $category->mark ?>
-<strong> Model: </strong> <?= $category->model ?>
-<strong> Engine: </strong> <?= $category->engine ?>
-<strong> Transmission: </strong> <?= $category->transmission ?>
+    <strong> Mark: </strong> <?= $category->mark ?>
+    <strong> Model: </strong> <?= $category->model ?>
+    <strong> Engine: </strong> <?= $category->engine ?>
+    <strong> Transmission: </strong> <?= $category->transmission ?>
 
-<a href="<?=Url::to(['/categories/view', 'id' => $category->id]) ?>" style="color:cyan"> Learn more! </a> 
-<br><br>
+    <a href="<?=Url::to(['/categories/view', 'id' => $category->id]) ?>" style="color:cyan"> Learn more! </a> 
+    <br><br>
     
 <?php } ?>
-
-
